47f4cdf3614cb271851dbe4d78f2cc15659ab6d2
chortex <romachorrt@gmail.com>
2021-10-26 03:17:06 +0300

    Fix Services section on Home page issues

--- src/Components/ServicesMain.js
+++ src/Components/ServicesMain.js
@@ -7,17 +7,17 @@ export default class ServicesMain extends Component {
     return (
       <Container className='pt-5 services-main-section'>
         <h1 className='display-3' style={{fontWeight: "600"}}>Services</h1>
-        <div className='pt-5 row'>
-          <a className='col-lg-3 col-md-6 main-bim-modeling main-services text-center' href="/services">
+        <div className='pt-5 row services-main-wrapper'>
+          <a className='col-3 main-bim-modeling main-services text-center' href="/services">
             BIM Modeling & Drafting
           </a>
-          <a className='col-lg-3 col-md-6 main-architectural-design main-services text-center' href="/services">
+          <a className='col-3 main-architectural-design main-services text-center' href="/services">
             Architectural Design
           </a>
-          <a className='col-lg-3 col-md-6 main-interior-design main-services text-center' href="/services">
+          <a className='col-3 main-interior-design main-services text-center' href="/services">
             Interior Design
           </a>
-          <a className='col-lg-3 col-md-6 main-architectural-visualization main-services text-center' href="/services">
+          <a className='col-3 main-architectural-visualization main-services text-center' href="/services">
             Architectural Visualization
           </a>
         </div>
